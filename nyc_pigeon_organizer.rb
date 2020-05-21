c6dc5f9cdd58284a47cb4b8fd132f334cc3a66cd
Berel Levy <berellevy@gmail.com>
2020-05-21 21:15:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,8 +3,8 @@ require "pry"
 def nyc_pigeon_organizer(data)
   organized_pigeon_nds = {}
   data.each do |category_key, details_hash|
-    binding.pry
     details_hash.each do |detail_key, names_array|
+      binding.pry
       names_array.each do |name_string|
 
         organized_pigeon_nds[name_string][category_key] = []
