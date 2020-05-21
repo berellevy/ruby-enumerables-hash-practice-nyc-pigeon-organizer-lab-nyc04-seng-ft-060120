233b1d82b44958b3cb3e568f3fe295e9d0fb82a0
Berel Levy <berellevy@gmail.com>
2020-05-21 21:06:42 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
+# category, detail, name
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized_pigeon_nds = {}
+  data.each do |category_key, details_array|
+    hash_of_details.each do |detail_key, names_array|
+      names_array.each do |name_string|
+
+        organized_pigeon_nds[name_string][category_key] = []
+        organized_pigeon_nds[name_string][category_key] << detail_key.to_s
+
+      end
+    end
+
+  end
 end
